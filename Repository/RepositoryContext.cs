69635f3bc0bb34a3f1149afe75c82e83840ba44b
Yakup Can ATALAR <atalaryakupcan@gmail.com>
2023-08-06 00:09:38 +0300

    Removed  unused usings

--- Repository/RepositoryContext.cs
+++ Repository/RepositoryContext.cs
@@ -1,11 +1,6 @@
 ﻿using Entities.Models;
 using Microsoft.EntityFrameworkCore;
 using Repository.Configuration;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Repository
 {
